fc6bb07486fffdd66af6cffe43b4bc69505b1c67
tonypai <tony77794@gmail.com>
2015-03-07 15:04:09 +0800

    include required modules and create empty index page

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello, idiomthon!");
+var express = require('express'),
+	app = express(),
+	http = require('http'),
+	server = http.createServer(app),
+	io = require('socket.io').listen(server, { log: false }),
+	fs = require('fs'),
+	port = (process.env.PORT || 5000);
+
+server.listen(port);
+
+console.log("server listening port "+port);
+
+app.use(express.static(__dirname + '/views'));
